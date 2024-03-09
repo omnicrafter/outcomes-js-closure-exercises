9122f932a2b195f249fe3e0a7071fc30821485e6
Omnicrafter <chen.jeffrey.a@gmail.com>
2024-03-08 22:13:10 -0600

    completed create account

--- create-account.js
+++ create-account.js
@@ -1,5 +1,49 @@
-function createAccount(pin, amount) {
+function createAccount(pin, amount = 0) {
+  let balance = amount;
+  let accountPin = pin;
 
+  return {
+    checkBalance: function (pin) {
+      if (pin !== accountPin) {
+        return "Invalid PIN.";
+      }
+
+      return `$${balance}`;
+    },
+    deposit: function (pin, amount) {
+      if (pin !== accountPin) {
+        return "Invalid PIN.";
+      }
+      if (typeof amount !== "number" || amount < 0) {
+        return "Please enter a valid amount.";
+      }
+      balance = balance + amount;
+      return `Successfully deposited $${amount}. Current balance: $${balance}.`;
+    },
+    withdraw: function (pin, amount) {
+      if (pin !== accountPin) {
+        return "Invalid PIN.";
+      }
+      if (typeof amount !== "number" || amount < 0) {
+        return "Please enter a valid amount.";
+      }
+
+      if (balance - amount < 0) {
+        return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+      }
+
+      balance = balance - amount;
+      return `Successfully withdrew $${amount}. Current balance: $${balance}.`;
+    },
+    changePin: function (pin, newPin) {
+      if (pin !== accountPin) {
+        return "Invalid PIN.";
+      }
+
+      accountPin = newPin;
+      return "PIN successfully changed!";
+    },
+  };
 }
 
 module.exports = { createAccount };
