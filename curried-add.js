b8ea50b723d97b6e27409dc42014ede35c51e6b0
Omnicrafter <chen.jeffrey.a@gmail.com>
2024-03-08 22:24:41 -0600

    completed curried add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,18 @@
-function curriedAdd(total) {
+function curriedAdd(num) {
+  let sum = 0;
 
+  function addNum(num) {
+    if (num === undefined) {
+      return sum;
+    }
+    sum += num;
+    return addNum;
+  }
+
+  if (num === undefined) {
+    return addNum();
+  }
+  return addNum(num);
 }
 
 module.exports = { curriedAdd };
